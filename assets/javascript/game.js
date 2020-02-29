0203378c951e11c6230779d83c1298b1f1d53a31
Frank Flores <frankthetank67@gmail.com>
2020-02-28 20:49:07 -0800

    using bootstrap classes in html and added styling for divs. added variables and array of Words to be guessed in js external file

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    // CREATE THE MISSING CODE HERE. Your code should add content to the random-number div.
+    // ...
+    $("#click-me").on("click", function() {
+      var randomNum = Math.floor(Math.random() * 1000) + 1;
+
+      $("#test-div").text(randomNum);
+    });
+
+    // ...
+
+    var vnumOfwins;
+    var stars = ["Big Dipper", "Scorpius", "Milky Way"];
+
+
+    function 
+
+    function fnumOfWins() {
+    	$('.num-of-wins').
+    	numOfWins++;
+    } 
+
+  });
+  
+
