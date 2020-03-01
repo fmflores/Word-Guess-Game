2a8681dcf7963f1d49ef48bdcf56d59d10552cfa
Frank Flores <frankthetank67@gmail.com>
2020-02-29 18:36:59 -0800

    created a gameStart function to initialize after every word has been guessed right or ran out of guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,100 @@
 $(document).ready(function() {
-    // example code for jquery use 
-    /*$("#click-me").on("click", function() {
-      var randomNum = Math.floor(Math.random() * 1000) + 1;
-
-      $("#test-div").text(randomNum);
-    });*/
-
+   
+    var wordToGuess;
+    var userGuess;
     // counter for numOfWins
-    var counter_wins = 0;
+    var counterWins = 0;
+    var losses = 0;
     //counter for remaining number of guesses
-    var counter_guesses = 0;
+    var numOfGuesses;
     // array of words to be guessed
     var spaceItems = ["Galaxy", "Orion", "Nebula", "Asteroid"];
     // variable of array to display the random word to be guessed (as " _ _ _ _ _")
      var blankWord = [];
+
+     var lettersGuessed = [];
+    
     // variable to randomize the word in the array
     var randomNum = Math.floor(Math.random() * spaceItems.length);
     var randomWord = spaceItems[randomNum];
-    console.log(randomNum + ' ' + randomWord);
 
+    var lettersLeftToGuess = randomWord.length;
+
+    console.log(randomNum + ' ' + randomWord);
+    function gameStart() {
+      blankWord = [];
+      $('#current-word').text("");
+      $('#num-of-wins').text(counterWins);
+      $('#guessesRemaining').text(numOfGuesses);
+      numOfGuesses = 5;
     // code for word being guessed turning into an array and displaying "fill in the blank" characters
     randomWord.split('').forEach(function(letter, i) {
         blankWord[i] = " _ ";
-        console.log(blankWord);
-
-        $('#current-word').append(blankWord);
+        lettersGuessed[i] = "_";
     });    
-
+    console.log(blankWord);
+    $('#current-word').append(blankWord);
+    $("#letters-guessed").text(lettersGuessed);
     // jquery event handler for when a key is pressed, if its the correct guess, populate this empty array with the correct character at the right index
+    wordToGuess = randomWord;
+    
+   }
+   gameStart();
+    $(document).keypress(function(e) {
+      if (numOfGuesses > 0) {
+      if (e.which !== 0) {
+          //alert("Character was typed. It was: " + String.fromCharCode(e.which));
+          userGuess = String.fromCharCode(e.which);
+          
+          
+          //console.log(e.which);
+           console.log(userGuess);
+         
+          lettersGuessed.forEach(function(item, itemIndex) {
+            if (lettersGuessed.length === itemIndex + 1) {
+              lettersGuessed[itemIndex] = userGuess + '_';
+            } else {
+              lettersGuessed[itemIndex] = userGuess;
+            }
+          });
+          $("#letters-guessed").text(lettersGuessed);
+          if (wordToGuess.includes(userGuess)) {
+            // console.log("this is a correct guess");
+          // console.log(wordToGuess.split(""));
+            wordToGuess.split("").forEach(function(item, itemIndex) {
+              //  console.log('On iteration ' + itemIndex + ' the letter of WordToGuess is ' + item);
+          
+              if (userGuess === item) {
+                blankWord[itemIndex] = userGuess;
+              } else {
+                // console.log('wrong guess');
+              }
+            });
+            
+            //console.log(blankWord.join(''));
+            $('#current-word').text(blankWord.join(''));
+
+            lettersLeftToGuess--;
+            if  (lettersLeftToGuess === 0) {
+              counterWins++;
+              gameStart();
 
 
-    /*$()
-    function 
+            }
 
-    function fnumOfWins() {
-    	$('.num-of-wins').
-    	numOfWins++;
-    } */
-
+          } else {
+            console.log("wrong");
+            numOfGuesses--;
+            $('#guessesRemaining').text(numOfGuesses);
+            if  (numOfGuesses === 0) {
+              losses++;
+              gameStart();
+            }
+            
+          }
+         } 
+      }
+  });
+  //console.log(userGuess);
   });
   
-
-/*var userGuess = "e";
-var blankWord = "_____".split("");
-console.log(blankWord.join(''));
-var wordToGuess = "peter";
-
-if (wordToGuess.includes(userGuess)) {
-  console.log("this is a correct guess");
-
-  wordToGuess.split("").forEach(function(item, itemIndex) {
-     console.log('On iteration ' + itemIndex + ' the letter of WordToGuess is ' + item);
-
-    if (userGuess === item) {
-      blankWord[itemIndex] = userGuess;
-    } else {
-      console.log('wrong guess');
-    }
-  });
-  
-  console.log(blankWord.join(''));
-}
-*/
